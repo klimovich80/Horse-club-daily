26dce19ee215a1b1efc52bc14267ceb5d0c95c89
Klim <admin@Air-Admin.IGD_Rostelecom>
2024-10-08 12:37:46 +0300

    создал схему для конюшни

---
+++ backend/models/staple.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+// схема конюшни
+const clientShema = new mongoose.Schema({
+  openedOn: Date, // дата открытия
+  name: String, // имя конюшни
+  adres: String, // адрес конюшни
+  website: String, // сайт конюшни
+  social: String, // социальные сети
+  phoneNumber: String, // телефон конюшни
+  comments: String, // комментарии
+  closed: Boolean, // закрыто ли конюшня
+}, { timestamps: true });
+
+module.exports = mongoose.model('staple', stapleShema);
